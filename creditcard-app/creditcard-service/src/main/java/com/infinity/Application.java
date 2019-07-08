6b207bff24e7678e3bfc808dcf6d88373128ec7e
Niraj <niraj.sehgal@bbc.co.uk>
2019-07-08 19:58:35 +0100

    Update Application.java
    Added missing import

--- creditcard-app/creditcard-service/src/main/java/com/infinity/Application.java
+++ creditcard-app/creditcard-service/src/main/java/com/infinity/Application.java
@@ -9,6 +9,7 @@ import java.util.stream.Stream;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
